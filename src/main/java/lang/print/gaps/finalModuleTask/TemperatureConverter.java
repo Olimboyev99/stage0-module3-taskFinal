4dae0c0c93660a81e477d3497e3db2d92b0e19e3
Hasanboy <hasanboyolimboyev40@gmail.com>
2022-12-20 17:30:27 +0500

    1

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.finalModuleTask;
 
+import java.util.Scanner;
+
 public class TemperatureConverter {
+    Scanner sc = new Scanner(System.in);
+    int c = sc.nextInt();
     public void toFahrenheit(int temperatureCelsius){
     }
 }
